91ffec2333d0e82b841675f7efc96154e323b1d3
ghostbuster91 <kghost0@gmail.com>
2017-11-13 12:52:16 +0100

    Fix import

--- app/src/main/java/io/ghostbuster91/android/react/component/example/login/ui/LoginActivity.kt
+++ app/src/main/java/io/ghostbuster91/android/react/component/example/login/ui/LoginActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import com.jakewharton.rxbinding2.view.enabled
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 import com.trello.rxlifecycle2.kotlin.bindToLifecycle
-import io.ghostbuster91.android.react.component.example.R
-import io.ghostbuster91.android.react.component.example.firstTypeAheadReducer
-import io.ghostbuster91.android.react.component.example.secondTypeAheadReducer
-import io.ghostbuster91.android.react.component.example.states
+import io.ghostbuster91.android.react.component.example.*
 import io.ghostbuster91.android.react.component.example.typeahead.TypeAhead
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.example_activity.*
@@ -17,7 +14,7 @@ class LoginActivity : RxAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.example_activity)
-        events().bindToLifecycle(this).subscribe(io.ghostbuster91.android.react.component.example.events)
+        uiEvents().bindToLifecycle(this).subscribe(events)
         states
                 .bindToLifecycle(this)
                 .publish()
@@ -30,7 +27,7 @@ class LoginActivity : RxAppCompatActivity() {
 
     }
 
-    private fun events(): Observable<TypeAhead.Event.TextChanged> {
+    private fun uiEvents(): Observable<TypeAhead.Event.TextChanged> {
         return Observable.merge(
                 typeAheadView.bindEvents(firstTypeAheadReducer.identifier),
                 secondTypeAheadView.bindEvents(secondTypeAheadReducer.identifier))
